ce5fe299ebab5877fa1f52466b5d6bc582c6fea1
juli5n <julian.kemp@outlook.de>
2026-01-02 14:19:37 +0100

    Added a structure that represents unsigned permutations that represent signed permutations

--- SSPRHannenhalliPevznerTheory/Basic.lean
+++ SSPRHannenhalliPevznerTheory/Basic.lean
@@ -3,6 +3,10 @@ import Mathlib.Tactic.Linarith
 import Mathlib.GroupTheory.Perm.Basic
 import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Tactic.IntervalCases
+import Mathlib.Data.Nat.SuccPred
+import Mathlib.Algebra.Group.Even
+import Mathlib.Order.Defs.PartialOrder
+
 -- set_option pp.coercions false
 -- set_option pp.all true
 
@@ -13,21 +17,28 @@ inductive Sign
   | negative
 deriving DecidableEq, Repr
 
-def Sign.toZ (s : Sign) : ℤ :=
+namespace Sign
+
+@[simp]
+def toZ (s : Sign) : ℤ :=
   match s with
   | positive => 1
   | negative => -1
 
-def Sign.toZeroOne (s : Sign) : Nat :=
+@[simp]
+def toZeroOne (s : Sign) : Nat :=
   match s with
   | positive => 1
   | negative => 0
 
 
-def Sign.flip (s : Sign) : Sign :=
+@[simp]
+def flip (s : Sign) : Sign :=
   match s with
   | positive => negative
   | negative => positive
+
+end Sign
 
 
 abbrev SignFunction (n : ℕ) := Fin n → Sign
@@ -39,6 +50,16 @@ instance : Inhabited Sign where
 structure SignedPermutation (n : ℕ) where
   values : Equiv.Perm (Fin n)
   signs : SignFunction n
+
+namespace SignedPermutation
+
+def identity (n : ℕ) : SignedPermutation (n := n) :=
+  {
+    values := Equiv.refl (Fin n),
+    signs := fun _ ↦ Sign.positive
+  }
+
+end SignedPermutation
 
 structure Reversal {n : ℕ} where
   start_index : Fin n
@@ -106,10 +127,18 @@ def TwoColoredGraph.fullGraph {n : ℕ} (two_colored_graph : TwoColoredGraph (n 
   two_colored_graph.blackEdgesGraph ⊔ two_colored_graph.grayEdgesGraph
 
 
-namespace BreakpointGraph
-
+@[simp]
 def isConsecutive {n : ℕ} (a b : Fin n) : Prop :=
 a.val = b.val + 1 ∨ b.val = a.val + 1
+
+def isConsecutive.refl {n : ℕ} (a b : Fin n) :
+isConsecutive a b ↔ isConsecutive b a := by
+  unfold isConsecutive
+  tauto
+
+
+namespace BreakpointGraph
+
 
 def fromPermutation {n : ℕ} (σ : Equiv.Perm (Fin n)) : TwoColoredGraph (n := n) :=
 {
@@ -156,8 +185,163 @@ def fromPermutation {n : ℕ} (σ : Equiv.Perm (Fin n))
 end CycleGraph
 
 
+structure UnsignedRepresentationOfSP {n : ℕ} where
+  val : Equiv.Perm (Fin (2*n))
+  property : ∀ (i : Fin n), isConsecutive (val ⟨2*i, by omega⟩) (val ⟨2*i+1, by omega⟩)
 
-def UnsignedPermutation.fromSigned {n : ℕ}
+def UnsignedRepresentationOfSP.property' {n : ℕ}
+  (unsigned_representation : UnsignedRepresentationOfSP (n := n)) :
+  ∀ (x : Fin (2*n)), ∀ (y : Fin (2*n)), x≠y → ((x.val/2) = (y.val/2)) →
+  isConsecutive (unsigned_representation.val x) (unsigned_representation.val y) := by
+  sorry
+
+
+#check Fin.rec
+#check Nat.strongRec
+#check le_trans
+#check Fin.induction
+
+
+def minOfPairIsEvenHelper
+{n : ℕ} (representation : UnsignedRepresentationOfSP (n := n)) :
+∀ (y₀ : Fin (2*n)),
+  let x₀ := representation.val.symm y₀
+  let x₁ : Fin (2*n) := ⟨if x₀.val % 2 = 0 then x₀.val + 1 else x₀.val - 1, by split <;> omega⟩
+  let y₁ := representation.val x₁
+  Even (min y₀.val y₁.val) := by
+  intro y₀
+  induction h : y₀.val generalizing y₀ with
+  | zero =>
+      intro x₀ x₁ y₁
+      have : min 0 y₁.val = 0 := by
+        apply le_antisymm
+        · exact min_le_left 0 y₁.val
+        · exact Nat.zero_le (min 0 y₁.val)
+      rw [this]
+      exact Nat.even_iff.mpr this
+  | succ i induction_hypothesis =>
+    intro x₀ x₁ y₁
+
+
+    have y₀_eq_representation_apply_x₀ : y₀ = representation.val x₀ :=
+      (Equiv.symm_apply_eq representation.val).mp rfl
+
+
+    let pair_index_x₀ : ℕ := x₀ / 2
+    let pair_index_x₁ : ℕ := x₁ / 2
+
+    have same_pair_index :pair_index_x₀ = pair_index_x₁ := by
+      dsimp [pair_index_x₀, pair_index_x₁, x₀, x₁]
+      split <;> omega
+
+    have x₀_neq_x₁ : x₀ ≠ x₁ := by
+      apply Fin.ne_of_val_ne
+      dsimp [x₀, x₁]
+      split_ifs with h1
+      <;> omega
+
+    have y₀_y₁_consecutive : isConsecutive y₀ y₁ := by
+      have := representation.property' x₀ x₁ x₀_neq_x₁ same_pair_index
+      rw [y₀_eq_representation_apply_x₀]
+      exact this
+
+    let min := min (i + 1) y₁.val
+    obtain min_is_y₁|min_is_ip1 := (min_choice (i+1) y₁).symm
+
+    -- If y₁ is the minimum, then y₁ must be i and we can apply
+    -- the induction hypothesis
+    · unfold isConsecutive at y₀_y₁_consecutive
+      have y₁_eq_i : y₁ = i := by omega
+
+      specialize induction_hypothesis y₁ y₁_eq_i
+
+      -- Rewrite the induction_hypothesis using the existing
+      -- bindings x₀, x₁, y₁ and y₂
+      dsimp only at induction_hypothesis
+
+      have preimage_y₁_eq_x₁ : (Equiv.symm representation.val) y₁ = x₁ :=
+        Equiv.symm_apply_apply representation.val x₁
+      simp only [preimage_y₁_eq_x₁] at induction_hypothesis
+
+      have x₀_eq_pair_neighbor_of_x₁ : x₀ = if x₁.val % 2 = 0 then x₁.val + 1 else x₁.val - 1 := by
+        split_ifs with h1
+        <;> omega
+      simp only [← x₀_eq_pair_neighbor_of_x₁, Fin.eta] at induction_hypothesis
+      have image_x₀_eq_y₀ : representation.val x₀ = y₀ :=
+        Eq.symm y₀_eq_representation_apply_x₀
+
+      rw [image_x₀_eq_y₀] at induction_hypothesis
+
+
+      -- !!I AM WORKING ON THIS PART!!
+
+
+      sorry
+    · by_contra min_is_odd
+      rw [Nat.not_even_iff_odd] at min_is_odd
+      sorry
+
+
+
+def UnsignedRepresentationOfSP.minOfPairIsEven
+{n : ℕ} (representation : UnsignedRepresentationOfSP (n := n)) :
+∀ (i : Fin n),
+let first : Fin (2*n) := representation.val ⟨2*i, by omega⟩
+let second : Fin (2*n) := representation.val ⟨2*i+1, by omega⟩
+Even (min first second).val := by
+  by_cases n_eq_0_case : n = 0
+  · intro i; have := i.isLt; linarith
+
+  have n_positive : 0 < n := Nat.pos_of_ne_zero n_eq_0_case
+
+  let min_of_corresponding_pair_even (y : Fin (2*n)) : Prop :=
+    let x₀ := representation.val.symm y
+    let x₁ : Fin (2*n) := ⟨x₀ + ((x₀.val + 1) % 2), by omega⟩
+    Even (min y.val (representation.val x₁).val)
+
+
+  have induction_step : ∀ (i : Fin (2*n)),
+    (∀ (j : Fin (2*n)), j < i → min_of_corresponding_pair_even j) →
+    min_of_corresponding_pair_even i := by
+    intro i induction_requirement
+    by_cases i_eq_0 : i.val = 0
+    · unfold min_of_corresponding_pair_even
+      intro x₀ x₁
+      have : min i.val (representation.val x₁).val = 0 := by
+        apply le_antisymm
+        · exact (le_trans
+            (min_le_left i.val (representation.val x₁))
+            (show i.val ≤ 0 by rw [i_eq_0])
+          )
+        · exact Nat.zero_le (min ↑i ↑(representation.val x₁))
+      rw [this]
+      exact Nat.even_iff.mpr rfl
+    · sorry
+
+    -- Beweis per Induktion:
+    -- Für 0 ist dies klar
+    -- Für n > 0: Angenommen das Minimum von representation x₀ und representation x₁
+    -- wäre nicht gerade.
+    -- Dann ist das minimum m ungerade
+    -- Dann ist m-1 gerade
+    -- Nach induktionsvoraussetzung ist m-1 in einem Paar enthalten dessen minimum
+    -- gerade ist.
+    -- Dann muss m-1 aber mit m zsm im Paar sein
+    -- Dann ist aber der andere Wert im Paar sowohl minimum als auch maximum
+
+
+
+
+
+
+
+  sorry
+
+
+instance {n : ℕ} : Coe (UnsignedRepresentationOfSP (n := n)) (Equiv.Perm (Fin (2*n))) where
+  coe x := x.val
+
+def SignedPermutation.toUnsignedDirect {n : ℕ}
 (signed_permutation : SignedPermutation (n := n)) :
 Equiv.Perm (Fin (2*n)) :=
   {
@@ -268,17 +452,79 @@ Equiv.Perm (Fin (2*n)) :=
     right_inv := by sorry
   }
 
-example {n : ℕ} (σ : SignedPermutation n) :
-  (BreakpointGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).blackEdgesGraph ≤
-  (CycleGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).blackEdgesGraph := by sorry
+def SignedPermutation.toUnsigned {n : ℕ}
+(signed_permutation : SignedPermutation n) :
+UnsignedRepresentationOfSP (n := n) :=
+  {
+    val := signed_permutation.toUnsignedDirect,
+    property := by
+      intro i
+      unfold isConsecutive
+      let sign_at_i : Sign := signed_permutation.signs i
+      have helper1: (2*i.val + 1) / 2 = i := by omega
+      have helper2: (2*i.val + 1 + 1) % 2 = 0 := by omega
+      cases sign_at_i_case : sign_at_i
+      · simp only [toUnsignedDirect, Sign.toZeroOne, Sign.flip, List.getElem_toArray,
+        Equiv.coe_fn_mk, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, mul_div_cancel_left₀,
+        Fin.eta, sign_at_i_case, add_zero, Nat.mul_mod_right, List.getElem_cons_zero, helper1,
+        Nat.mul_add_mod_self_left, Nat.mod_succ, List.getElem_cons_succ,
+        or_true, sign_at_i]
+      · simp only [toUnsignedDirect, Sign.toZeroOne, Sign.flip, List.getElem_toArray,
+        Equiv.coe_fn_mk, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, mul_div_cancel_left₀,
+        Fin.eta, sign_at_i_case, Nat.mul_add_mod_self_left, Nat.mod_succ, List.getElem_cons_succ,
+        List.getElem_cons_zero, helper1, helper2, true_or, sign_at_i]
+  }
 
-example {n : ℕ} (σ : SignedPermutation n) :
-  (BreakpointGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).grayEdgesGraph ≤
-  (CycleGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).grayEdgesGraph := by sorry
+#check Equiv.Perm.inv_eq_iff_eq
+#check Nat.div_eq_iff
+#check Order.le_and_le_succ_iff
 
-example {n : ℕ} (σ : SignedPermutation n) :
-  (BreakpointGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).fullGraph ≤
-  (CycleGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).fullGraph := by sorry
+example : (⟨0, by norm_num⟩ : Fin 2).val = 0 := by exact rfl
+
+def SignedPermutation.fromUnsigned {n : ℕ}
+(unsigned_permutation : UnsignedRepresentationOfSP (n := n)) :
+SignedPermutation (n := n) :=
+  {
+    values := {
+      toFun :=
+        fun i ↦
+        ⟨(unsigned_permutation.val ⟨2*i, by omega⟩) / 2, by omega⟩
+      invFun :=
+        fun i ↦
+        ⟨unsigned_permutation.val.invFun ⟨2*i, by omega⟩ / 2, by omega⟩
+      left_inv := by
+        intro i
+        simp only
+        apply Fin.eq_of_val_eq
+        rw [Nat.div_eq_iff (by norm_num : 0 < 2)]
+        simp only [Equiv.invFun_as_coe, Nat.add_one_sub_one]
+        rw [Nat.le_and_le_add_one_iff]
+
+        sorry
+
+      right_inv := sorry
+    },
+    signs :=
+      fun i ↦
+        let first := unsigned_permutation.val ⟨2*i, by omega⟩
+        let second := unsigned_permutation.val ⟨2*i+1, by omega⟩
+        if first < second then Sign.positive else Sign.negative
+  }
+
+
+-- example {n : ℕ} (σ : SignedPermutation n) :
+--   (BreakpointGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).blackEdgesGraph ≤
+--   (CycleGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).blackEdgesGraph := by sorry
+
+-- example {n : ℕ} (σ : SignedPermutation n) :
+--   (BreakpointGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).grayEdgesGraph ≤
+--   (CycleGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).grayEdgesGraph := by sorry
+
+-- example {n : ℕ} (σ : SignedPermutation n) :
+--   (BreakpointGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).fullGraph ≤
+--   (CycleGraph.fromPermutation (UnsignedPermutation.fromSigned σ)).fullGraph := by sorry
+
+
 
 def test_SP1 : SignedPermutation 4 := {
   values := Equiv.refl (Fin 4)
@@ -294,9 +540,9 @@ def test_SP3 : SignedPermutation 4 := {
   signs := fun _ ↦ Sign.positive
 }
 
-def result_UP1 := UnsignedPermutation.fromSigned test_SP1
-def result_UP2 := UnsignedPermutation.fromSigned test_SP2
-def result_UP3 := UnsignedPermutation.fromSigned test_SP3
+def result_UP1 := test_SP1.toUnsignedDirect
+def result_UP2 := test_SP2.toUnsignedDirect
+def result_UP3 := test_SP3.toUnsignedDirect
 
 
 #eval! (List.range 8).map (fun i => result_UP1 (Fin.ofNat (2*4) i))
@@ -317,6 +563,27 @@ def result_UP3 := UnsignedPermutation.fromSigned test_SP3
   let x := Fin.ofNat 8 i
   result_UP3.symm (result_UP3 x)
 )
+
+def SignedPermutation.sortable {n : ℕ} (π : SignedPermutation (n := n)) :
+∃ reversal_list : List (Reversal (n := n)),
+(SignedPermutation.identity n) = (reversal_list.foldl SignedPermutation.applyReversal π) := by
+  sorry
+
+
+
+noncomputable def SignedPermutation.reversalDistance
+{n : ℕ} (π : SignedPermutation (n := n)) : ℕ :=
+  have exists_k_st_π_is_sortable_in_k_steps : ∃ (k : ℕ), ∃ reversal_list : List (Reversal (n := n)),
+  reversal_list.length = k ∧
+  (SignedPermutation.identity n) = (reversal_list.foldl SignedPermutation.applyReversal π) := by
+    obtain ⟨reversal_list, _⟩ := π.sortable
+    use reversal_list.length
+    use reversal_list
+  open Classical in Nat.find exists_k_st_π_is_sortable_in_k_steps
+
+
+
+
 
 
 end SSPRHannenhalliPevznerTheory.Basic
